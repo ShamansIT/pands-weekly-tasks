43a01576ff6441544bf948b7c3fd6f7fb5bb7689
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-02-06 19:04:09 +0000

    Week 3 Lab1 complete

---
+++ Weekly Tasks 03/Lab3.1.2_Sub.py
@@ -0,0 +1,6 @@
+# Program to subtract on number from another.
+
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+calculate = a-b
+print("{} minus {} is {} ".format(a, b, calculate))
