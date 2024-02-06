8bb4f42d6e372c22dc08266e83d606e6e828b611
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-02-06 19:10:20 +0000

    Revert "Week 3 Lab1 complete"
    This reverts commit 43a01576ff6441544bf948b7c3fd6f7fb5bb7689.

--- Weekly Tasks 03/Lab3.1.2_Sub.py
+++
@@ -1,6 +0,0 @@
-# Program to subtract on number from another.
-
-a = int(input("Enter 1st number: "))
-b = int(input("Enter 2nd number: "))
-calculate = a-b
-print("{} minus {} is {} ".format(a, b, calculate))
