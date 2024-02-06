5321db41a5faa55535de4be749a10c4f2d56422c
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-02-06 20:24:13 +0000

    week3 Complete

---
+++ Weekly Tasks 03/accounts.py
@@ -0,0 +1,11 @@
+# Bank account hide number
+
+# Prompt the user to enter an account number
+acc_number = input("Input accoutn number more then 4 digits: ")
+
+# Check if the account number length is greater than 4 and is digit
+if len(acc_number) < 4 or not acc_number.isdigit():
+    print(f"Error input! Number validation error in number \"{acc_number}\"")
+else:
+    convert_acc_str = 'x' * (len(acc_number) - 4) + acc_number[-4:]
+    print(convert_acc_str)
