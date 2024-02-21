f3c50dc6cd21f8fa1ef4effca49f18d0adb0f579
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-02-21 13:39:23 +0000

    Weekly Task 05 weekday.py Completed

---
+++ Weekly Tasks 05/Lab/weekday.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+# Get the current date and time
+now = datetime.now()
+
+# Extract the day of the week (0=Monday, 6=Sunday)
+day_of_week = now.strftime('%A')
+
+# Init dict for week day
+weekDay = {
+    'Monday': 'Sad day',  # Often seen as the start of the work/school week.
+    'Tuesday': 'Busy day',  # The productivity kicks in.
+    'Wednesday': 'Hump day',  # Midweek, the peak of the workload.
+    'Thursday': 'Hopeful day',  # The anticipation for the weekend begins.
+    'Friday': 'Happy day',  # The excitement for the weekend is high.
+    'Saturday': 'Relaxing day',  # Typically a day for leisure and relaxation.
+    # Often a day for family gatherings and rest before the week starts again.
+    'Sunday': 'Family day',
+}
+
+# Print conclusion
+print(f"The {day_of_week} is a", weekDay[day_of_week])
