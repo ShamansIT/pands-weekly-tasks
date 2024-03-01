991a2594f048d4bf365856c97c791d2aba7c7542
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-03-01 12:20:21 +0000

    Lab6.1_week06_function Completed

--- Weekly Tasks 06/Lab6.1_week06_function.py
+++ Weekly Tasks 06/Lab6.1_week06_function.py
@@ -1,29 +1,49 @@
+def readModules():
+    modules = []
+    moduleName = input(
+        "\tEnter the first Module name (blank to quit) :").strip()
 
-def add():
-    print("ADD")
-    pass
+    while moduleName != "":
+        module = {}
+        module["name"] = moduleName
+        module["grade"] = int(input("\t\tEnter grade:"))
+        modules.append(module)
+        moduleName = input(
+            "\tEnter next module name (blank to quit) :").strip()
+    return modules
 
 
-def view():
-    print("VIEW")
-    pass
+def add(students):
+    currentStudent = {}
+    currentStudent["name"] = input("Enter name :")
+    currentStudent["modules"] = readModules()
+    students.append(currentStudent)
 
 
-def quit():
-    print("QUIT")
-    pass
+def view(students):
+    for currentStudent in students:
+        print(currentStudent["name"])
+        displayModules(currentStudent["modules"])
 
+
+def displayModules(modules):
+    print("\tName \tGrade")
+    for module in modules:
+        print(f"\t{ module['name']} \t{ module['grade']}")
+
+
+students = []
 
 while True:
     print("\nA -> ADD\tV -> VIEW\tQ -> QUIT")
     choise = input("\nMake your choise:  ")
     print(f"You choose {choise}")
     if choise.upper() == 'A':
-        add()
+        add(students)
     elif choise.upper() == 'V':
         view()
     elif choise.upper() == 'Q':
-        print("PROGRAM COMPLETED!")
+        print("PROGRAM COMPLETED! QUIT!")
         break
     else:
         print("WRONG ENTER! Please try again", end="\n")
