ea007e4f37a7f1df6b300470400dbdde82ef9ecd
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-03-05 21:14:06 +0000

    Lab 7 in progress

---
+++ Weekly Tasks 07/Lab/count.py
@@ -0,0 +1,22 @@
+FILENAME = "Weekly Tasks 07\Lab\count.txt"
+
+
+def readNumber():
+    with open(FILENAME) as f:
+        number = int(f.read())
+        return number
+
+
+# test it
+num = readNumber()
+print(num)
+
+
+def writeNumber(number):
+    with open(FILENAME, "wt") as f:
+        # write takes a string so we need to convert
+        f.write(str(number))
+
+
+# test it
+writeNumber(3)
---
+++ Weekly Tasks 07/Lab/run_count.py
@@ -0,0 +1,20 @@
+FILENAME = "Weekly Tasks 07\Lab\count.txt"
+
+
+def readNumber():
+    with open(FILENAME) as f:
+        number = int(f.read())
+    return number
+
+
+def writeNumber(number):
+    with open(FILENAME, "wt") as f:
+        # write takes a string so we need to convert
+        f.write(str(number))
+
+
+# main
+num = readNumber()
+num += 1
+print(f"we have run this program {num} times")
+writeNumber(num)
