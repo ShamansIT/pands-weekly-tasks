28ae8837f466ceeb1a0893de59e294447a746aee
Serhii Spitsyn <serhii.spitsyn.ie@gmail.com>
2024-03-05 21:03:07 +0000

    Week 7 Assigment Completed

---
+++ Weekly Tasks 07/es.py
@@ -0,0 +1,20 @@
+import os
+
+file_name = input("Please enter file name(best is: moby-dick.txt) -> ")
+letter = input("Please enter letter to check -> ")
+
+# Format path
+name_strip = "Weekly Tasks 07\\" + file_name.strip()
+
+
+if name_strip[-4:] != '.txt':  # Check file exicution
+    print("Wrong file extention. Not *.txt")
+elif os.path.exists(name_strip):  # Check is file exists
+    count = 0
+    with open(name_strip, 'rt') as f:
+        for line in f:  # Read each line
+            line_to_count = line  # Count symbol in each line
+            count = count + line.count(letter)  # Count symbol
+    print(f"Count letter {letter} is {count}")
+else:
+    print(file_name.strip(), "does not exist! Wrong enter!")
